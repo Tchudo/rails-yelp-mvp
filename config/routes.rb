5c155e1a669e4cf3ca1ce8a8ebe5af428c4e2861
Tchudo <gael.lebreton@gmail.com>
2020-02-12 15:52:46 +0100

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "restaurants", to: 'restaurants#index'
+  get "restaurants/new", to: 'restaurants#new', as:'new'
+  post "restaurants", to: 'restaurants#create'
+  get "restaurants/:id", to: 'restaurants#show', as:'restaurant'
+
+  get "restaurants/:restaurant_id/reviews/new", to: 'reviews#new', as: 'new_review'
+  post "restaurants/:restaurant_id/reviews", to: 'reviews#create', as: 'restaurant_reviews'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'start'
+  # Restaurant.destroy_all
+
+puts 'empty'
+  Pizza = Restaurant.create!(name: "Pizza Nico", address: "145 cours du Medoc", category: "chinese", phone_number: "0200000000")
+  Restaurant.create!(name: "Bar des Sables", address: "sur le vieux port", category: "italian", phone_number: "0200000002")
+  Restaurant.create!(name: "Cafe Porh Guerh", address: "9 rue des anseaux", category: "french", phone_number: "0200000003")
+  Restaurant.create!(name: "Le Brise Lame", address: "3 place de la mairie", category: "japanese", phone_number: "0200000004")
+  Restaurant.create!(name: "Creperie Le clos Marine", address: "Plouhinec", category: "belgian", phone_number: "0200000001")
+
+puts 'restaurants done'
+  Review.create!(restaurant: Pizza, content: "Excellent!", rating: 5)
+puts 'reviews done'
+puts 'finish'
